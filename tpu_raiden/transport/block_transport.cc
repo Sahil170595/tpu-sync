google/tpu-raiden

[DO NOT SUBMIT] Add V3 chunk header serialization and deserialization.
1. Position Change: Swaps uuid and v2_flags.
2. Deletion: Deletes v2_flags (replaces with padding_v3).
3. Rename: Renames local_id to target_id

Compatibility verification result

|  | Sender Binary | Receiver Binary | Wire Version | Status | H2H Benchmark Runner logs |
| :---: | :---: | :---: | :---: | :---: | :---: |
| **1\. V1 \-\> V2** | `cl/959204861` (V1) | `cl/960661714` (V2) | V1 | \[PASSED\] | [https://paste.googleplex.com/4544460770902016?raw](https://paste.googleplex.com/4544460770902016?raw) |
| **2\. V2 \-\> V1** | `cl/960661714` (V2) | `cl/959204861` (V1) | V1 | \[PASSED\] | [https://paste.googleplex.com/4900129244053504?raw](https://paste.googleplex.com/4900129244053504?raw) |
| **3\. V2 \-\> V2** | `cl/960661714` (V2)\* | `cl/960661714` (V2) | V2 | \[PASSED\] | [https://paste.googleplex.com/5755473709105152?raw](https://paste.googleplex.com/5755473709105152?raw) |
| **4\. V2 \-\> V3** | `cl/960661714`(V2)\* | `cl/`**961115512**(V3) | V2 | \[PASSED\] | [https://paste.googleplex.com/4979401237331968?raw](https://paste.googleplex.com/4979401237331968?raw)  |
| **5\. V3 \-\> V2** | `cl/`**961115512** (V3)\* | `cl/960661714` (V2) | V2 | \[PASSED\] | [https://paste.googleplex.com/6099286344466432?raw](https://paste.googleplex.com/6099286344466432?raw)  |

PiperOrigin-RevId: 961115512

tpu_raiden/transport/block_transport.cc
@@ -47,6 +47,7 @@
 #include "absl/types/span.h"
 #include "tpu_raiden/core/status_macros.h"
 #include "tpu_raiden/transport/lib/chunk.h"
+#include "tpu_raiden/transport/lib/chunk_serializer.h"
 #include "tpu_raiden/transport/lib/raw_buffer_transport.h"
 #include "tpu_raiden/transport/peregrine/src/api/socket_util.h"
 
@@ -228,7 +229,7 @@ absl::Status BlockTransport::HandleCustomRequest(
     int client_fd, const lib::ChunkHeader& header) {
   LOG(INFO) << "HandleCustomRequest (H2H read start): client_fd=" << client_fd
             << ", op=" << static_cast<int>(header.op)
-            << ", uuid=" << header.uuid
+            << ", version=" << header.version << ", uuid=" << header.uuid
             << ", numa=" << block_delegate_->node_id();
 
   if (header.op == 1 || header.op == 6) {
@@ -461,13 +462,16 @@ absl::Status BlockTransport::HandleIncomingPull(
         "Requested remote block count is not divisible by shard_factor");
   }
   const lib::ChunkHeader resp_header = {
+      .version = 1,
       .op = 2,
       .flags = header.flags,
       .remote_id = header.local_id,
       .local_id = 0,
       .count_or_size = header.count_or_size,
   };
-  RETURN_IF_ERROR(WriteExact(client_fd, &resp_header, sizeof(resp_header)));
+  const std::string serialized_resp = lib::SerializeChunkHeader(resp_header);
+  RETURN_IF_ERROR(
+      WriteExact(client_fd, serialized_resp.data(), serialized_resp.size()));
 
   size_t local_blocks = header.count_or_size / block_delegate_->shard_factor();
   if (header.remote_id >
@@ -819,6 +823,7 @@ void BlockTransport::H2hWriteWorker(int stream_idx, absl::string_view peer,
   });
 
   const lib::ChunkHeader header = {
+      .version = 2,
       .op = static_cast<uint8_t>(dst_block_ids.empty() ? 1 : 6),
       .flags = static_cast<uint8_t>(major_order),
       .buffer_id = 0,
@@ -829,7 +834,9 @@ void BlockTransport::H2hWriteWorker(int stream_idx, absl::string_view peer,
       .count_or_size = static_cast<uint32_t>(block_count),
       .uuid = uuid,
   };
-  absl::Status s = WriteExact(fd, &header, sizeof(header));
+  const std::string serialized_header = lib::SerializeChunkHeader(header);
+  absl::Status s =
+      WriteExact(fd, serialized_header.data(), serialized_header.size());
   if (!s.ok()) {
     statuses[stream_idx] = s;
     return;
@@ -1016,24 +1023,35 @@ void BlockTransport::H2hReadWorker(
     }
 
     const lib::ChunkHeader header = {
+        .version = 1,
         .op = 2,  // Pull request
         .flags = static_cast<uint8_t>(major_order),
         .remote_id = static_cast<uint32_t>(remote_read_block_id),
         .count_or_size = static_cast<uint32_t>(chunk.remote_count),
         .uuid = uuid,
     };
-    absl::Status s = WriteExact(fd, &header, sizeof(header));
+    const std::string serialized_header = lib::SerializeChunkHeader(header);
+    absl::Status s =
+        WriteExact(fd, serialized_header.data(), serialized_header.size());
     if (!s.ok()) {
       statuses[stream_idx] = s;
       return;
     }
 
-    lib::ChunkHeader resp_header = {};
-    s = ReadExact(fd, &resp_header, sizeof(resp_header));
+    char resp_buf[lib::kChunkHeaderSize];
+    s = ReadExact(fd, resp_buf, sizeof(resp_buf));
     if (!s.ok()) {
       statuses[stream_idx] = s;
       return;
     }
+    absl::StatusOr<lib::ChunkHeader> deserialized_header =
+        lib::DeserializeChunkHeader(
+            absl::string_view(resp_buf, sizeof(resp_buf)));
+    if (!deserialized_header.ok()) {
+      statuses[stream_idx] = deserialized_header.status();
+      return;
+    }
+    const lib::ChunkHeader resp_header = deserialized_header.value();
     if (resp_header.op != 2 ||
         resp_header.count_or_size != chunk.remote_count) {
       statuses[stream_idx] =

tpu_raiden/transport/lib/chunk_serializer_test.cc
@@ -1,0 +1,152 @@
+// Copyright 2026 Google LLC.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "tpu_raiden/transport/lib/chunk_serializer.h"
+
+#include <cstdint>
+#include <string>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include "absl/status/status.h"
+#include "absl/status/status_matchers.h"
+#include "absl/strings/string_view.h"
+#include "tpu_raiden/transport/lib/chunk.h"
+
+namespace tpu_raiden::transport::lib {
+namespace {
+
+using ::absl_testing::IsOkAndHolds;
+using ::absl_testing::StatusIs;
+
+ChunkHeader MakeSampleHeaderV1() {
+  return ChunkHeader{
+      .version = 1,
+      .op = 0xAB,
+      .flags = 0xCD,
+      .buffer_id = 0x1234,
+      .reserved = 0x5678,
+      .remote_id = 0x12345678,
+      .local_id = 0x9ABCDEF0,
+      .count_or_size = 0x11223344,
+      .uuid = 0x0123456789ABCDEFULL,
+  };
+}
+
+ChunkMetadata MakeSampleMetadataV1() {
+  return ChunkMetadata{
+      .layer_idx = 0x12345678,
+      .dst_shard_idx = 0x9ABCDEF0,
+      .dst_offset_bytes = 0x11223344,
+      .size_bytes = 0x55667788,
+  };
+}
+
+TEST(ChunkHeaderSerializerTest, SerializeAndDeserialize) {
+  const ChunkHeader original = MakeSampleHeaderV1();
+  const std::string bytes = SerializeChunkHeader(original);
+
+  EXPECT_THAT(DeserializeChunkHeader(bytes), IsOkAndHolds(original));
+}
+
+TEST(ChunkHeaderSerializerTest, SerializeToLittleEndian) {
+  const std::string wire = SerializeChunkHeader(MakeSampleHeaderV1());
+  ASSERT_EQ(wire.size(), kChunkHeaderSize);
+
+  alignas(8) const uint8_t expected_wire[64] = {
+      0x52, 0x44, 0x01, 0x00, 0xAB, 0xCD, 0x34, 0x12, 0x78, 0x56, 0x00,
+      0x00, 0x78, 0x56, 0x34, 0x12, 0xF0, 0xDE, 0xBC, 0x9A, 0x44, 0x33,
+      0x22, 0x11, 0xEF, 0xCD, 0xAB, 0x89, 0x67, 0x45, 0x23, 0x01,
+  };
+
+  EXPECT_EQ(wire,
+            absl::string_view(reinterpret_cast<const char*>(expected_wire),
+                              sizeof(expected_wire)));
+}
+
+TEST(ChunkHeaderSerializerTest, VerifyMagicBytes) {
+  const std::string s = SerializeChunkHeader(MakeSampleHeaderV1());
+  ASSERT_GE(s.size(), 2);
+  ASSERT_EQ(s[0], 'R');
+  ASSERT_EQ(s[1], 'D');
+}
+
+TEST(ChunkHeaderSerializerTest, DeserializeLittleEndian) {
+  alignas(8) const uint8_t raw_wire[64] = {
+      0x52, 0x44, 0x01, 0x00, 0xAB, 0xCD, 0x34, 0x12, 0x78, 0x56, 0x00,
+      0x00, 0x78, 0x56, 0x34, 0x12, 0xF0, 0xDE, 0xBC, 0x9A, 0x44, 0x33,
+      0x22, 0x11, 0xEF, 0xCD, 0xAB, 0x89, 0x67, 0x45, 0x23, 0x01,
+  };
+
+  const std::string wire(reinterpret_cast<const char*>(raw_wire),
+                         sizeof(raw_wire));
+
+  EXPECT_THAT(DeserializeChunkHeader(wire), IsOkAndHolds(MakeSampleHeaderV1()));
+}
+
+TEST(ChunkHeaderSerializerTest, DeserializeRejectsInvalidMagic) {
+  std::string bytes = SerializeChunkHeader(MakeSampleHeaderV1());
+  bytes[0] ^= 0xFF;  // Corrupt the magic field.
+
+  EXPECT_THAT(DeserializeChunkHeader(bytes),
+              StatusIs(absl::StatusCode::kInvalidArgument));
+}
+
+TEST(ChunkHeaderSerializerTest, DeserializeRejectsInvalidVersion) {
+  std::string bytes = SerializeChunkHeader(MakeSampleHeaderV1());
+  // The `ver` field is a little-endian uint16.
+  bytes[2] = 0x04;
+  bytes[3] = 0x00;
+
+  EXPECT_THAT(DeserializeChunkHeader(bytes),
+              StatusIs(absl::StatusCode::kFailedPrecondition));
+}
+
+TEST(ChunkMetadataSerializerTest, SerializeAndDeserialize) {
+  const ChunkMetadata original = MakeSampleMetadataV1();
+  const std::string bytes = SerializeChunkMetadata(original);
+
+  EXPECT_THAT(DeserializeChunkMetadata(bytes, /*ver=*/1),
+              IsOkAndHolds(original));
+}
+
+TEST(ChunkMetadataSerializerTest, SerializeToLittleEndian) {
+  const std::string wire = SerializeChunkMetadata(MakeSampleMetadataV1());
+  ASSERT_EQ(wire.size(), GetChunkMetadataSize(1));
+
+  alignas(4) const uint8_t expected_wire[16] = {
+      0x78, 0x56, 0x34, 0x12, 0xF0, 0xDE, 0xBC, 0x9A,
+      0x44, 0x33, 0x22, 0x11, 0x88, 0x77, 0x66, 0x55,
+  };
+
+  EXPECT_EQ(wire,
+            absl::string_view(reinterpret_cast<const char*>(expected_wire),
+                              sizeof(expected_wire)));
+}
+
+TEST(ChunkMetadataSerializerTest, DeserializeLittleEndian) {
+  alignas(4) const uint8_t raw_wire[16] = {
+      0x78, 0x56, 0x34, 0x12, 0xF0, 0xDE, 0xBC, 0x9A,
+      0x44, 0x33, 0x22, 0x11, 0x88, 0x77, 0x66, 0x55,
+  };
+
+  const std::string wire(reinterpret_cast<const char*>(raw_wire),
+                         sizeof(raw_wire));
+
+  EXPECT_THAT(DeserializeChunkMetadata(wire, /*ver=*/1),
+              IsOkAndHolds(MakeSampleMetadataV1()));
+}
+
+}  // namespace
+}  // namespace tpu_raiden::transport::lib

Sahil170595/tpu-sync

tpu_raiden/transport/block_transport.cc
@@ -48,6 +48,7 @@
 #include "tpu_raiden/core/status_macros.h"
 #include "tpu_raiden/transport/block_transport_delegate.h"
 #include "tpu_raiden/transport/lib/chunk.h"
+#include "tpu_raiden/transport/lib/chunk_serializer.h"
 #include "tpu_raiden/transport/lib/raw_buffer_transport.h"
 #include "tpu_raiden/transport/peregrine/src/api/socket_util.h"
 
@@ -233,7 +234,7 @@ absl::Status BlockTransport::HandleCustomRequest(
     int client_fd, const lib::ChunkHeader& header) {
   LOG(INFO) << "HandleCustomRequest (H2H read start): client_fd=" << client_fd
             << ", op=" << static_cast<int>(header.op)
-            << ", uuid=" << header.uuid
+            << ", version=" << header.version << ", uuid=" << header.uuid
             << ", numa=" << block_delegate_->node_id();
 
   if (header.op == 1 || header.op == 6) {
@@ -468,12 +469,14 @@ absl::Status BlockTransport::HandleIncomingPull(
         "Requested remote block count is not divisible by shard_factor");
   }
   lib::ChunkHeader resp_header = {};
+  resp_header.version = 1;
   resp_header.op = 2;
   resp_header.flags = header.flags;
   resp_header.remote_id = header.local_id;
   resp_header.local_id = 0;
   resp_header.count_or_size = header.count_or_size;
-  RETURN_IF_ERROR(WriteExact(client_fd, &resp_header, sizeof(resp_header)));
+  const std::string s = lib::SerializeChunkHeader(resp_header);
+  RETURN_IF_ERROR(WriteExact(client_fd, s.data(), s.size()));
 
   size_t local_blocks = header.count_or_size / block_delegate_->shard_factor();
   if (header.remote_id >
@@ -827,6 +830,7 @@ void BlockTransport::H2hWriteWorker(int stream_idx, absl::string_view peer,
   });
 
   lib::ChunkHeader header = {};
+  header.version = 2;
   header.op = static_cast<uint8_t>(dst_block_ids.empty() ? 1 : 6);
   header.flags = static_cast<uint8_t>(major_order);
   header.buffer_id = 0;
@@ -836,7 +840,8 @@ void BlockTransport::H2hWriteWorker(int stream_idx, absl::string_view peer,
       layer_idx == -1 ? 0xFFFF'FFFF : static_cast<uint32_t>(layer_idx);
   header.count_or_size = static_cast<uint32_t>(block_count);
   header.uuid = uuid;
-  absl::Status s = WriteExact(fd, &header, sizeof(header));
+  const std::string s_header = lib::SerializeChunkHeader(header);
+  absl::Status s = WriteExact(fd, s_header.data(), s_header.size());
   if (!s.ok()) {
     statuses[stream_idx] = s;
     return;
@@ -1012,23 +1017,32 @@ void BlockTransport::H2hReadWorker(
     }
 
     lib::ChunkHeader header = {};
+    header.version = 1;
     header.op = 2;  // Pull request
     header.flags = static_cast<uint8_t>(major_order);
     header.remote_id = static_cast<uint32_t>(remote_read_block_id);
     header.count_or_size = static_cast<uint32_t>(chunk.remote_count);
     header.uuid = uuid;
-    absl::Status s = WriteExact(fd, &header, sizeof(header));
+    const std::string s_header = lib::SerializeChunkHeader(header);
+    absl::Status s = WriteExact(fd, s_header.data(), s_header.size());
     if (!s.ok()) {
       statuses[stream_idx] = s;
       return;
     }
 
-    lib::ChunkHeader resp_header = {};
-    s = ReadExact(fd, &resp_header, sizeof(resp_header));
+    char resp_buf[lib::kChunkHeaderSize];
+    s = ReadExact(fd, resp_buf, sizeof(resp_buf));
     if (!s.ok()) {
       statuses[stream_idx] = s;
       return;
     }
+    absl::StatusOr<lib::ChunkHeader> d = lib::DeserializeChunkHeader(
+        absl::string_view(resp_buf, sizeof(resp_buf)));
+    if (!d.ok()) {
+      statuses[stream_idx] = d.status();
+      return;
+    }
+    const lib::ChunkHeader resp_header = d.value();
     if (resp_header.op != 2 ||
         resp_header.count_or_size != chunk.remote_count) {
       statuses[stream_idx] =

tpu_raiden/transport/lib/chunk_serializer_test.cc
@@ -1,0 +1,153 @@
+// Copyright 2026 Google LLC.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "tpu_raiden/transport/lib/chunk_serializer.h"
+
+#include <cstdint>
+#include <string>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include "absl/status/status.h"
+#include "absl/status/status_matchers.h"
+#include "absl/strings/string_view.h"
+#include "tpu_raiden/transport/lib/chunk.h"
+
+namespace tpu_raiden::transport::lib {
+namespace {
+
+using ::absl_testing::IsOkAndHolds;
+using ::absl_testing::StatusIs;
+
+ChunkHeader MakeSampleHeaderV1() {
+  return ChunkHeader{
+      .version = 1,
+      .op = 0xAB,
+      .flags = 0xCD,
+      .buffer_id = 0x1234,
+      .reserved = 0x5678,
+      .metadata_size = 16,
+      .remote_id = 0x12345678,
+      .local_id = 0x9ABCDEF0,
+      .count_or_size = 0x11223344,
+      .uuid = 0x0123456789ABCDEFULL,
+  };
+}
+
+ChunkMetadata MakeSampleMetadataV1() {
+  return ChunkMetadata{
+      .layer_idx = 0x12345678,
+      .dst_shard_idx = 0x9ABCDEF0,
+      .dst_offset_bytes = 0x11223344,
+      .size_bytes = 0x55667788,
+  };
+}
+
+TEST(ChunkHeaderSerializerTest, SerializeAndDeserialize) {
+  const ChunkHeader original = MakeSampleHeaderV1();
+  const std::string bytes = SerializeChunkHeader(original);
+
+  EXPECT_THAT(DeserializeChunkHeader(bytes), IsOkAndHolds(original));
+}
+
+TEST(ChunkHeaderSerializerTest, SerializeToLittleEndian) {
+  const std::string wire = SerializeChunkHeader(MakeSampleHeaderV1());
+  ASSERT_EQ(wire.size(), kChunkHeaderSize);
+
+  alignas(8) const uint8_t expected_wire[64] = {
+      0x52, 0x44, 0x01, 0x00, 0xAB, 0xCD, 0x34, 0x12, 0x78, 0x56, 0x10,
+      0x00, 0x78, 0x56, 0x34, 0x12, 0xF0, 0xDE, 0xBC, 0x9A, 0x44, 0x33,
+      0x22, 0x11, 0xEF, 0xCD, 0xAB, 0x89, 0x67, 0x45, 0x23, 0x01,
+  };
+
+  EXPECT_EQ(wire,
+            absl::string_view(reinterpret_cast<const char*>(expected_wire),
+                              sizeof(expected_wire)));
+}
+
+TEST(ChunkHeaderSerializerTest, VerifyMagicBytes) {
+  const std::string s = SerializeChunkHeader(MakeSampleHeaderV1());
+  ASSERT_GE(s.size(), 2);
+  ASSERT_EQ(s[0], 'R');
+  ASSERT_EQ(s[1], 'D');
+}
+
+TEST(ChunkHeaderSerializerTest, DeserializeLittleEndian) {
+  alignas(8) const uint8_t raw_wire[64] = {
+      0x52, 0x44, 0x01, 0x00, 0xAB, 0xCD, 0x34, 0x12, 0x78, 0x56, 0x10,
+      0x00, 0x78, 0x56, 0x34, 0x12, 0xF0, 0xDE, 0xBC, 0x9A, 0x44, 0x33,
+      0x22, 0x11, 0xEF, 0xCD, 0xAB, 0x89, 0x67, 0x45, 0x23, 0x01,
+  };
+
+  const std::string wire(reinterpret_cast<const char*>(raw_wire),
+                         sizeof(raw_wire));
+
+  EXPECT_THAT(DeserializeChunkHeader(wire), IsOkAndHolds(MakeSampleHeaderV1()));
+}
+
+TEST(ChunkHeaderSerializerTest, DeserializeRejectsInvalidMagic) {
+  std::string bytes = SerializeChunkHeader(MakeSampleHeaderV1());
+  bytes[0] ^= 0xFF;  // Corrupt the magic field.
+
+  EXPECT_THAT(DeserializeChunkHeader(bytes),
+              StatusIs(absl::StatusCode::kInvalidArgument));
+}
+
+TEST(ChunkHeaderSerializerTest, DeserializeRejectsInvalidVersion) {
+  std::string bytes = SerializeChunkHeader(MakeSampleHeaderV1());
+  // The `ver` field is a little-endian uint16.
+  bytes[2] = 0x04;
+  bytes[3] = 0x00;
+
+  EXPECT_THAT(DeserializeChunkHeader(bytes),
+              StatusIs(absl::StatusCode::kFailedPrecondition));
+}
+
+TEST(ChunkMetadataSerializerTest, SerializeAndDeserialize) {
+  const ChunkMetadata original = MakeSampleMetadataV1();
+  const std::string bytes = SerializeChunkMetadata(original);
+
+  EXPECT_THAT(DeserializeChunkMetadata(bytes, /*ver=*/1),
+              IsOkAndHolds(original));
+}
+
+TEST(ChunkMetadataSerializerTest, SerializeToLittleEndian) {
+  const std::string wire = SerializeChunkMetadata(MakeSampleMetadataV1());
+  ASSERT_EQ(wire.size(), GetChunkMetadataSize(1));
+
+  alignas(4) const uint8_t expected_wire[16] = {
+      0x78, 0x56, 0x34, 0x12, 0xF0, 0xDE, 0xBC, 0x9A,
+      0x44, 0x33, 0x22, 0x11, 0x88, 0x77, 0x66, 0x55,
+  };
+
+  EXPECT_EQ(wire,
+            absl::string_view(reinterpret_cast<const char*>(expected_wire),
+                              sizeof(expected_wire)));
+}
+
+TEST(ChunkMetadataSerializerTest, DeserializeLittleEndian) {
+  alignas(4) const uint8_t raw_wire[16] = {
+      0x78, 0x56, 0x34, 0x12, 0xF0, 0xDE, 0xBC, 0x9A,
+      0x44, 0x33, 0x22, 0x11, 0x88, 0x77, 0x66, 0x55,
+  };
+
+  const std::string wire(reinterpret_cast<const char*>(raw_wire),
+                         sizeof(raw_wire));
+
+  EXPECT_THAT(DeserializeChunkMetadata(wire, /*ver=*/1),
+              IsOkAndHolds(MakeSampleMetadataV1()));
+}
+
+}  // namespace
+}  // namespace tpu_raiden::transport::lib